8e2f63ecf1a9512bc465a87a76ec55918cac93ee
xuyujie <xuyujie@zjhcsoft.com>
2024-03-08 17:10:15 +0800

    0308

---
+++ suanfati/index.js
@@ -0,0 +1,13 @@
+//1、冒泡排序
+let arr = [16, 31, 12, 1, 9, 12, 10];
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+            }
+        }
+    }
+    return arr
+}
+bubbleSort(arr)

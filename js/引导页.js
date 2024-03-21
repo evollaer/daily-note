d9904a068c407a59a492b43b6af2114f29bbb088
xuyujie <xuyujie@zjhcsoft.com>
2024-03-21 14:39:29 +0800

    引导页

---
+++ js/引导页.js
@@ -0,0 +1,29 @@
+// https://driverjs.com/
+// import { driver } from 'driver.js';
+// import 'driver.js/dist/driver.css';
+
+// driver({
+//     showProgress: true,
+//     steps: [
+//         {
+//             popover: {
+//                 title: 'Welcome',
+//                 description: 'Hello World! 👋',
+//             },
+//         },
+//         {
+//             element: `.${prefixVar}-layout-header-trigger`,
+//             popover: {
+//                 title: 'Collapse Button',
+//                 description: 'This is the menu collapse button.',
+//             },
+//         },
+//         {
+//             element: `.${prefixVar}-layout-header-action`,
+//             popover: {
+//                 title: 'User Action',
+//                 description: 'This is the user function area.',
+//             },
+//         },
+//     ],
+// }).drive();

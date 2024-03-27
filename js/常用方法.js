bf32f3bac9f7834d1ff9c34bba68a17d2d659b51
xuyujie <xuyujie@zjhcsoft.com>
2024-03-27 11:12:09 +0800

    0327

--- js/常用方法.js
+++ js/常用方法.js
@@ -5,4 +5,32 @@ function trim(str) {
 
 String.prototype.trim = function () {
     return this.replace(/(^\s*)|(\s*$)/g, "");
+};
+
+// 获取url后面的参数
+function getQuery() {
+    var loc = decodeURI(location.href); //URL里的汉字出现乱码使用decodeURI方法解码
+    let urlStr = loc.split("?")[1];
+    if (!urlStr) {
+        return;
+    }
+    let object = {};
+    let paramsArr = urlStr.split("&");
+    for (let i = 0, len = paramsArr.length; i < len; i++) {
+        let arr = paramsArr[i].split("=");
+        object[arr[0]] = arr[1];
+    }
+    return object;
+}
+
+// 时间戳转换-》2024-01-01 00:00:00
+export function formatDate(value) {
+    let date = new Date(value);
+    let y = date.getFullYear();
+    let MM = (date.getMonth() + 1).toString().padStart(2, '0');
+    let d = date.getDate().toString().padStart(2, '0');
+    let h = date.getHours().toString().padStart(2, '0');
+    let m = date.getMinutes().toString().padStart(2, '0');
+    let s = date.getSeconds().toString().padStart(2, '0');
+    return `${y}-${MM}-${d} ${h}:${m}:${s}`;
 }
--- suanfati/index.js
+++ suanfati/index.js
@@ -11,3 +11,42 @@ function bubbleSort(arr) {
     return arr
 }
 bubbleSort(arr)
+
+// 旅程最优化-贪心算法
+const spots = [
+    { name: '故宫', time: 1, score: 7 },
+    { name: '颐和园', time: 2, score: 8 },
+    { name: '长城', time: 3, score: 9 },
+    { name: '天坛', time: 1, score: 6 }
+];
+
+function getMaxValueSpots(maxTime, spots) {
+    // 计算每个景点的单位时间价值
+    const valueSpots = spots.map(spot => ({
+        name: spot.name,
+        value: spot.score / spot.time
+    }));
+
+    // 按照单位时间价值从大到小排序
+    valueSpots.sort((a, b) => b.value - a.value);
+
+    let totalTime = 0;
+    const maxValueSpots = [];
+    for (let i = 0; i < valueSpots.length; i++) {
+        const spot = spots.find(item => item.name === valueSpots[i].name);
+        if (totalTime + spot.time <= maxTime) {
+            // 如果还有时间，加入当前景点
+            totalTime += spot.time;
+            maxValueSpots.push(spot.name);
+        } else {
+            // 没有时间了，退出循环
+            break;
+        }
+    }
+
+    return maxValueSpots;
+}
+
+const maxTime = 4;
+const result = getMaxValueSpots(maxTime, spots);
+console.log(result); // ["长城", "颐和园"]
